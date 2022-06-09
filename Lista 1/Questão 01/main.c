00c8b8546eceaeda33e613980a7c9a1e1418f63e
iamdavy <106548054+iamdavy@users.noreply.github.com>
2022-06-09 13:01:09 -0300

    Lista 1 update

---
+++ Lista 1/Questão 01/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+
+    printf("Digite um numero inteiro\n");
+    scanf("%i",&num);
+
+    printf("O numero e %i", num);
+    return 0;
+}
